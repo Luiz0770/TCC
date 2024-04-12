d119c653f107111d646ff03a086c2e654eebe18f
Luiz Felipe <ramosluizfelipe104@gmail.com>
2024-04-12 02:41:11 -0300

    finalizar demo do TCC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+  canActivate,
+} from '@angular/fire/auth-guard'
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo([''])
+const redirectLoggedInToHome = () => redirectLoggedInTo(['tipofamilia'])
 
 const routes: Routes = [
   {
@@ -9,7 +17,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome)
   },
   {
     path: 'metas',
@@ -26,7 +35,28 @@ const routes: Routes = [
   {
     path: 'ganhos',
     loadChildren: () => import('./pages/ganhos/ganhos.module').then( m => m.GanhosPageModule)
-  }
+  },
+  {
+    path: 'folder',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'tipofamilia',
+    loadChildren: () => import('./pages/tipofamilia/tipofamilia.module').then( m => m.TipofamiliaPageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
+  },
+  {
+    path: 'iniciokid',
+    loadChildren: () => import('./pages/iniciokid/iniciokid.module').then( m => m.IniciokidPageModule)
+  },
+  {
+    path: 'recsenha',
+    loadChildren: () => import('./pages/recsenha/recsenha.module').then( m => m.RecsenhaPageModule)
+  },
 ];
 
 @NgModule({
